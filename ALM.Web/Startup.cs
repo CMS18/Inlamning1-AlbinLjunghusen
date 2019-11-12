fd6426d45d005e7a7c630faed18e66b2c963c624
Albin Ljunghusen <albinljunghusen@gmail.com>
2019-11-12 12:26:09 +0100

    Added Cool List

--- ALM.Web/Startup.cs
+++ ALM.Web/Startup.cs
@@ -24,6 +24,8 @@ namespace ALM.Web
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddSingleton<BankRepository>();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
