585014d5202a46f8052bd86983d0e693f43cdab2
Albin Ljunghusen <albinljunghusen@gmail.com>
2019-11-12 11:14:53 +0100

    Initial

---
+++ ALM.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ALM.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
