d12eabe3a34655ac0008f871473653a0a381537c
Albin Ljunghusen <albinljunghusen@gmail.com>
2019-11-13 10:22:01 +0100

    arghg

--- ALM.Web/Exceptions/InvalidTransactionException.cs
+++ ALM.Web/Exceptions/InvalidTransactionException.cs
@@ -1,11 +1,12 @@
 ﻿using System;
+//DETTA E ETT FULHAX!   
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace ALM.Web
 {
-    public class InvalidTransactionException:Exception
+    public class InvalidTransactionException : Exception
     {
         public InvalidTransactionException(string message) : base(message)
         {
