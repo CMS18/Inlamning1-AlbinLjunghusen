96da019a82c72f7ccac4cf6e0ec4943aec59308d
rickard uttermalm <rickard.uttermalm@gmail.com>
2019-11-13 17:36:33 +0100

    Added transfer function with unit tests

---
+++ ALM.Web/Exceptions/InvalidTransferException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ALM.Web.Exceptions
+{
+    public class InvalidTransferException : Exception
+    {
+        public InvalidTransferException(string message) : base(message)
+        {
+
+        }
+    }
+}
---
+++ ALM.Web/Models/TransferViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ALM.Web.Models
+{
+    public class TransferViewModel
+    {
+        [Required]
+        [Display(Name = "From Account")]
+        public int FromAccountId { get; set; }
+        [Required]
+        [Display(Name = "To Account")]
+        public int ToAccountId { get; set; }
+
+        [Required]
+        [Display(Name = "Amount")]
+        [DataType(DataType.Currency)]
+        public decimal Amount { get; set; } 
+    }
+}
